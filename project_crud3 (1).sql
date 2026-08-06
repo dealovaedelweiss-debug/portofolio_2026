e0b46f2ff183375bed16665da7d8f423bbf49838
edelweiss <ppkdjwebdealovaedelweiss@gmail.com>
2026-08-06 20:28:43 +0700

    Add files via upload

--- project_crud3 (1).sql
+++ project_crud3 (1).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 01, 2026 at 10:02 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Waktu pembuatan: 06 Agu 2026 pada 15.28
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `contacts`
+-- Struktur dari tabel `blog`
+--
+
+CREATE TABLE `blog` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` text DEFAULT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT 1,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `blog`
+--
+
+INSERT INTO `blog` (`id`, `date`, `image`, `title`, `description`, `is_active`, `created_at`, `updated_at`) VALUES
+(1, '2026-08-04', '6a7198a94453e_komputer.jpeg', 'Komputer', 'ini adalah komputer', 1, '2026-08-04 07:45:45', NULL),
+(2, '2026-08-05', '6a719a5e960f5_program.jpeg', 'Program', 'ini adalah pemrograman', 1, '2026-08-04 07:53:02', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `contacts`
 --
 
 CREATE TABLE `contacts` (
@@ -37,10 +62,44 @@ CREATE TABLE `contacts` (
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data untuk tabel `contacts`
+--
+
+INSERT INTO `contacts` (`id`, `name`, `email`, `subject`, `message`, `created_at`, `updated_at`) VALUES
+(1, 'edel', 'edel@gmail.com', 'kirimpwsan', 'edel', '2026-08-05 01:46:17', NULL),
+(2, 'edel', 'el@gmail.com', 'ada pesan', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum nemo obcaecati corrupti dicta provident, officiis amet earum itaque quibusdam debitis at magni, possimus, dolorem vel! Aut dolore quibusdam nobis in.', '2026-08-05 02:04:04', NULL);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `resume`
+-- Struktur dari tabel `project`
+--
+
+CREATE TABLE `project` (
+  `id` int(11) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `link` varchar(50) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `subtitle` varchar(50) NOT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT 1,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `project`
+--
+
+INSERT INTO `project` (`id`, `image`, `link`, `title`, `subtitle`, `is_active`, `created_at`, `updated_at`) VALUES
+(3, '6a74719f79990_pyhton.png', 'https://docs.google.com/document/d/1ujSY703SzFPF0j', 'Sistem Kasir ', 'Pengembangan sistem kasir menggunakan bahasa pemro', 1, '2026-08-06 11:35:59', '2026-08-06 12:29:19'),
+(4, '6a7480ae55ca1_bmi2.jpg', 'https://drive.google.com/file/d/1E15jTCLUW72jrU7J_', 'Kalkulator BMI', 'Pengembangan Kalkulator Kesehatan Implementasi Jav', 1, '2026-08-06 11:38:23', '2026-08-06 12:40:14'),
+(5, '6a747d8f128fd_Picture1.png', 'https://drive.google.com/file/d/10VN7Z4eLADy36wDBG', 'APLIKASI ARRAY DINAMIS: APLIKASI RESERVASI KAMAR H', 'Project UAS struktur data yang mengimplementasikan', 1, '2026-08-06 12:26:55', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `resume`
 --
 
 CREATE TABLE `resume` (
@@ -55,17 +114,19 @@ CREATE TABLE `resume` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `resume`
+-- Dumping data untuk tabel `resume`
 --
 
 INSERT INTO `resume` (`id`, `title`, `year_start`, `year_end`, `substitle`, `description`, `created_at`, `updated_at`) VALUES
-(1, 'Teknik & Informatika', '2024', '2025', 'Informatic', 'Mahasiswa informatika semester 8', '2026-08-01 07:25:26', '2026-08-01 07:59:34'),
-(2, 'mesin', '2019', '2022', 'teknik mesin', 'askdsaik', '2026-08-01 07:59:57', NULL);
+(4, 'Cleaning Service', '2025', '2025', 'Mal Artha Gading', '', '2026-08-06 10:58:50', '2026-08-06 11:11:41'),
+(5, 'Beauty Therapist', '2025', '2025', 'Oriskin', '', '2026-08-06 10:59:26', '2026-08-06 11:11:35'),
+(6, 'BPH', '2025', '2027', 'Phos Creative Visual', 'Bendahara, Humas & Talent', '2026-08-06 11:00:13', '2026-08-06 11:11:14'),
+(7, 'Operator Komputer', '2024', '2024', 'PPKD Jakarta Pusat', '', '2026-08-06 11:04:49', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `settings`
+-- Struktur dari tabel `settings`
 --
 
 CREATE TABLE `settings` (
@@ -81,16 +142,39 @@ CREATE TABLE `settings` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `settings`
+-- Dumping data untuk tabel `settings`
 --
 
 INSERT INTO `settings` (`id`, `website_name`, `email`, `phone`, `address`, `description`, `ig`, `created_at`, `updated_at`) VALUES
-(1, 'www.opi.com', 'opi@gmail.com', '085695225', 'jakarta', 'oke', 'opiganteng', '2026-07-30 02:50:26', '2026-07-30 03:22:01');
+(1, 'Dealova Edelweiss', 'dealovaedelweissss@gmail.com', '085892089968', 'Sunter, Jakarta Utara, DKI Jakarta', 'Mahasiswa Informatika semester 3 di UBSI yang sedang fokus mendalami web programming. Memadukan logika IT, pengalaman mengurus organisasi, dan kecintaan pada dunia visual untuk membangun website yang ', 'edlnoirss', '2026-07-30 02:50:26', '2026-08-06 10:21:38');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sliders`
+-- Struktur dari tabel `skills`
+--
+
+CREATE TABLE `skills` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `progress` int(3) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `skills`
+--
+
+INSERT INTO `skills` (`id`, `name`, `progress`, `created_at`, `updated_at`) VALUES
+(4, 'Pyhton', 50, '2026-08-06 10:34:52', NULL),
+(5, 'Java', 50, '2026-08-06 10:35:02', NULL),
+(6, 'English', 50, '2026-08-06 10:35:20', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `sliders`
 --
 
 CREATE TABLE `sliders` (
@@ -109,17 +193,16 @@ CREATE TABLE `sliders` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `sliders`
+-- Dumping data untuk tabel `sliders`
 --
 
 INSERT INTO `sliders` (`id`, `title`, `substitle`, `description`, `button1_text`, `button1_link`, `button2_text`, `button2_link`, `image`, `is_active`, `created_at`, `updated_at`) VALUES
-(8, 'james', 'member of cortis', 'halo', 'horor', 'https://youtu.be/pFVlqX1N8Y4?si=5IB58xtvDIaJmztm', 'komedi', 'https://youtu.be/U6BDbXIah-Y?si=nrMK7b8vY86OJRwr', '6a6d94759c685_202521314489245202.jpeg', 1, '2026-08-01 06:38:01', '2026-08-01 06:38:45'),
-(9, 'james cortis', 'member of cortis', 'hello', 'like me', 'https://youtu.be/pFVlqX1N8Y4?si=5IB58xtvDIaJmztm', 'love me', 'https://youtu.be/pFVlqX1N8Y4?si=5IB58xtvDIaJmztm', '6a6d94bf74b86_62065301110561805.jpeg', 1, '2026-08-01 06:39:59', NULL);
+(12, 'Dealova Edelweiss', 'Informatika', 'Universitas Bina Sarana Informatika', 'Instagram', 'https://www.instagram.com/edlnoirs?igsh=MW4zd3drZX', 'Linkedln', 'https://www.linkedin.com/in/dealova-edelweiss-34b5', '6a745d5687eaa_home3.png', 1, '2026-08-06 10:09:26', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -130,7 +213,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
+-- Dumping data untuk tabel `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
@@ -144,65 +227,102 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
 --
 
 --
--- Indexes for table `contacts`
+-- Indeks untuk tabel `blog`
+--
+ALTER TABLE `blog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `contacts`
 --
 ALTER TABLE `contacts`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `resume`
+-- Indeks untuk tabel `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `resume`
 --
 ALTER TABLE `resume`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `settings`
+-- Indeks untuk tabel `settings`
 --
 ALTER TABLE `settings`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `sliders`
+-- Indeks untuk tabel `skills`
+--
+ALTER TABLE `skills`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `sliders`
 --
 ALTER TABLE `sliders`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `contacts`
+-- AUTO_INCREMENT untuk tabel `blog`
 --
-ALTER TABLE `contacts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `resume`
---
-ALTER TABLE `resume`
+ALTER TABLE `blog`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `settings`
+-- AUTO_INCREMENT untuk tabel `contacts`
+--
+ALTER TABLE `contacts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `project`
+--
+ALTER TABLE `project`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `resume`
+--
+ALTER TABLE `resume`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `settings`
 --
 ALTER TABLE `settings`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `sliders`
+-- AUTO_INCREMENT untuk tabel `skills`
 --
-ALTER TABLE `sliders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+ALTER TABLE `skills`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT untuk tabel `sliders`
+--
+ALTER TABLE `sliders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
